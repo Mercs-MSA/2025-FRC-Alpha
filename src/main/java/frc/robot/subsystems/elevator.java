538fbaa01bc978b441a15894de58957082b0a012
richar2048 <richarai2048@gmail.com>
2025-01-18 13:11:12 -0600

    first committ

--- src/main/java/frc/robot/subsystems/elevator.java
+++ src/main/java/frc/robot/subsystems/elevator.java
@@ -7,13 +7,17 @@ public class elevator extends SubsystemBase
 {
     private final PWMSparkMax elevatorMotor = new PWMSparkMax(0);
     elevatorMotor.setVelocity(); //initalize the motor to some value
-    private int elevatorOffset = 10; //need to find an offset value
+    private int elevatorOffset; //need to find an offset value
     //if some button(button1) is pressed, then set the motor to some value
     //the elevator goes up to a specific position
+    //if button1 pressed, go to l1 pos
+    //if button2 pressed, go to l2 pos
+    //if button3 pressed, go to l3 pos
+    //if button4 pressed, go to l4 pos
 
     //create two buttons that adjust the elevator position by an offset
-    //if button2 pressed, move current elevator position by +offset
-    //if button3 pressed, move current elevator position by -offset
+    //if button5 pressed, move current elevator position by +offset
+    //if button6 pressed, move current elevator position by -offset
 
 
 }
