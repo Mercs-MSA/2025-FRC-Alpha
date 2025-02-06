dfa378b73d9d5d418cbdf16293496fe6971810d8
lawrencetanFTC <lawrencetan458@gmail.com>
2025-02-05 18:42:42 -0600

    Evan climber (#2)
    * First Commit evan
    
    * Second Commit
    
    * Evan, Third Commit Coral intake/outake finished
    
    * Joshua added a reusable limit switch class
    
    * WCP Bot Metho Preparations
    
    * Map done
    
    * Evan Pivot mostly complete
    
    * Evan finished pivot, started elevator
    
    * Evan Elevator progress
    
    * this is nothing dont worry
    
    ---------
    
    Co-authored-by: EvanOsi <evan.o@mckinneysteamacademy.org>
    Co-authored-by: Joshua Lee <joshua.l@mckinneysteamacademy.org>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,6 +3,12 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import frc.robot.Constants.MotorConstants.AvailableState;
+import frc.robot.Constants.PivotConstants;
 
 import com.ctre.phoenix6.CANBus;
 
@@ -15,10 +21,77 @@ import com.ctre.phoenix6.CANBus;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
+  
+
+  public static class MotorConstants {
+    public static final int Flywheelintake = 1;
+    public static final int IntakePivot = 2;
+    public static final int ElevatorMain = 3;
+    public static final int ElevatorFollower = 4;
+    public static final int CoralIntakeRPM = 10;
+
+    public static String state = "Move";
+
+     public enum AvailableState {
+      LEVEL1(10.0, 10.0),
+      LEVEL2(20.0, 20.0),
+      LEVEL3(30.0, 30.0),
+      LEVEL4(40.0, 40.0);
+
+      private double pivotPos;
+      private double elevatorPos;
+
+      private AvailableState(Double pivotPos, Double elevatorPos)
+      {
+        this.pivotPos = pivotPos;
+        this.elevatorPos = elevatorPos;
+     }
+
+      public double pivotPosGet() {
+        return this.pivotPos;
+      };
+
+      public double elevatorPosGet() {
+        return this.elevatorPos;
+      };
+     }
+
+
+    // public static final Map<AvailableState,Double> pivotMotorPositions = new HashMap<AvailableState,Double>() {{
+    //   put(AvailableState.LEVEL1, 10.0);
+    //   put(AvailableState.LEVEL2, 20.0);
+    //   put(AvailableState.LEVEL3, 30.0);
+    //   put(AvailableState.LEVEL4, 40.0);
+
+    //   //Pivot motor position is dependent on elevator position,
+    //   //instead could be a mode for pivot from operator side that checks if elevator position is ok
+    //   // put(AvailableState.ALGAE, 50.0);
+    //   // put("MoveToProcessor", 60.0);
+    //   // put("MoveToBarge", 70.0);
+    // }};
+
+    //MoveToIntakePosition - Number state (constants)
+    //MoveToCoral1 - Number state (constants)
+    //MoveToCoral2and3 - Number state (constants)
+    //MoveToCoral4 - Number state (constants)
+
+    //MoveToAlgae - Number state (constants)
+    //MoveToProcessor - Number state (constants)
+    //MoveToBarge - Number state (constants)
+  }
+
+
+
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
 
+  public class PivotConstants
+  {
+      public static AvailableState pivotState = AvailableState.LEVEL1;
+  }
+  
   public static class ClawConstants {
     public static int clawMotorID = 0;
     public static int rangerID = 0;
---
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -0,0 +1,96 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.MotorConstants.AvailableState;
+import frc.robot.Constants.PivotConstants;
+
+public class Pivot extends SubsystemBase {
+  private final TalonFX pivotMotor = new TalonFX(Constants.MotorConstants.IntakePivot, "canivore");
+  
+  private final PositionVoltage pivotVoltage = new PositionVoltage(0);
+
+  private boolean isMoving = false;
+
+
+
+  public Pivot() {
+    TalonFXConfiguration configs = new TalonFXConfiguration();
+    configs.Slot0.kP = 2.4; // An error of 1 rotation results in 2.4 V output
+    configs.Slot0.kI = 0; // No output for integrated error
+    configs.Slot0.kD = 0.1; // A velocity of 1 rps results in 0.1 V output
+    // Peak output of 8 V
+    configs.Voltage.withPeakForwardVoltage(8)
+      .withPeakReverseVoltage(-8);
+
+    configs.Slot1.kP = 60; // An error of 1 rotation results in 60 A output
+    configs.Slot1.kI = 0; // No output for integrated error
+    configs.Slot1.kD = 6; // A velocity of 1 rps results in 6 A output
+    // Peak output of 120 A
+    configs.TorqueCurrent.withPeakForwardTorqueCurrent(40)
+      .withPeakReverseTorqueCurrent(-40);
+
+    /* Retry config apply up to 5 times, report if failure */
+    StatusCode status = StatusCode.StatusCodeNotInitialized;
+    for (int i = 0; i < 5; ++i) {
+      status = pivotMotor.getConfigurator().apply(configs);
+      if (status.isOK()) break;
+    }
+    if (!status.isOK()) {
+      System.out.println("Could not apply configs, error code: " + status.toString());
+    }
+
+    /* Make sure we start at 0 */
+    pivotMotor.setPosition(0);
+    PivotConstants.pivotState = AvailableState.LEVEL1;
+  }
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+
+
+
+  //MoveToIntakePosition - Number state (constants)
+  //MoveToCoral1 - Number state (constants)
+  //MoveToCoral2and3 - Number state (constants)
+  //MoveToCoral4 - Number state (constants)
+
+  //MoveToAlgae - Number state (constants)
+  //MoveToProcessor - Number state (constants)
+  //MoveToBarge - Number state (constants)
+
+
+  //Moving - Boolean? (within file)
+  //Moving used in initialization of command
+
+  //int CurrentMotorPos
+  //String DesiredMotorPos - will call to a map in constants for motor position number
+  public void moveMethod(Double DesiredMotorPos) {
+    pivotMotor.setControl(pivotVoltage.withPosition(DesiredMotorPos));
+    //pivotMotor.setControl(pivotVoltage.withPosition(Constants.MotorConstants.pivotMotorPositions.get(DesiredMotorPos (AvailableState type) )));
+  }
+
+  public void motorArrived() {
+    PivotConstants.pivotState = AvailableState.LEVEL2;
+  };
+
+  // private boolean isMovingCheck() {
+  //   return isMoving;
+  // };
+
+  // private void isMovingSet(Boolean update) {
+  //   isMoving = update;
+  // };
+}
