94fbf52ecd12529e559d17f84232b93eacf18847
avery <awartist2020@gmail.com>
2025-02-05 18:44:45 -0600

    Limelight code in robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -42,7 +42,7 @@ public class Robot extends TimedRobot {
 
       LimelightHelpers.SetRobotOrientation("limelight", m_robotContainer.drivetrain.getState().Pose.getRotation().getDegrees(), 0, 0, 0, 0, 0);
       LimelightHelpers.PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
-      if(Math.abs(m_gyro.getRate()) > 720) // if our angular velocity is greater than 720 degrees per second, ignore vision updates
+      if(Math.abs(m_robotContainer.drivetrain.getPigeon2().getAngularVelocityZWorld().getValueAsDouble()) > 720) // if our angular velocity is greater than 720 degrees per second, ignore vision updates
       {
         doRejectUpdate = true;
       }
@@ -53,9 +53,7 @@ public class Robot extends TimedRobot {
       if(!doRejectUpdate)
       {
         m_robotContainer.drivetrain.setVisionMeasurementStdDevs(VecBuilder.fill(.7,.7,9999999));
-        m_robotContainer.drivetrain.addVisionMeasurement(
-            mt2.pose,
-            mt2.timestampSeconds);
+        m_robotContainer.drivetrain.addVisionMeasurement(mt2.pose,mt2.timestampSeconds);
       }
 
     // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
