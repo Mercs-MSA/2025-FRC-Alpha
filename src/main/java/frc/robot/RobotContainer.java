970e0e68639892c5f5d08a6338a35a84b06fba54
LawrenceTanFTC <lawrencetan458@gmail.com>
2025-02-05 19:37:50 -0600

    Revert "Merge branch 'main' into Lawrence"
    This reverts commit 115e261fa5379c0bdaa99f8ea829d145dfee0933, reversing
    changes made to 4b546032e87e643f7dfd9f6984f56152f1f9d3e6.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,29 +5,12 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.Constants.MotorConstants.AvailableState;
 import frc.robot.commands.Autos;
-import frc.robot.commands.CoralCommand;
 import frc.robot.commands.ExampleCommand;
-import frc.robot.generated.TunerConstants;
-import frc.robot.subsystems.CommandSwerveDrivetrain;
-// import frc.robot.commands.FlipIntakeCommand;
-// import frc.robot.commands.IntakeCommand;
-// import frc.robot.commands.OuttakeCommand;
-// import frc.robot.subsystems.CoralIntake;
 import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.commands.CommandPivotPos;
-import frc.robot.commands.CommandSetState;
-import frc.robot.subsystems.Coral;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -37,16 +20,11 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  // private final CoralIntake m_CoralIntake = new CoralIntake();
-  // private final IntakeCommand m_IntakeCommand= new IntakeCommand(m_CoralIntake);
-  private final Coral m_coral = new Coral();
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
-  
-  public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
-
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -64,25 +42,13 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // m_driverController.a().onTrue(new IntakeCommand(m_CoralIntake));
-    // m_driverController.b().onTrue(new OuttakeCommand(m_CoralIntake));
-    // m_driverController.x().onTrue(new FlipIntakeCommand(m_CoralIntake));
-
-
-
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    // new Trigger(m_coral::exampleCondition)
-    //     .onTrue(new ExampleCommand(m_coral));
+    new Trigger(m_exampleSubsystem::exampleCondition)
+        .onTrue(new ExampleCommand(m_exampleSubsystem));
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.y().whileTrue(new CoralCommand(m_coral, -1));
-    m_driverController.b().whileTrue(new CoralCommand(m_coral, 1));
-    m_driverController.a().onTrue(new SequentialCommandGroup(
-      new CommandSetState(AvailableState.LEVEL2),
-      new CommandPivotPos()
-    ));
-
+    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
   /**
@@ -90,12 +56,8 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  //public Command getAutonomousCommand() { //Dont know what to do with this for now, commenting it out.
+  public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    // return Autos.exampleAuto(m_exampleSubsystem);
-  //}
-  // public Command getAutonomousCommand() {
-  //  An example command will be run in autonomous
-  //  return Autos.exampleAuto(m_exampleSubsystem);
-  // }
+    return Autos.exampleAuto(m_exampleSubsystem);
+  }
 }
--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -284,5 +284,3 @@ public class TunerConstants {
         }
     }
 }
-
-}
