760fa5b9a41ae8739fa85c24e1ff5237adc0a3f4
lawrencetanFTC <lawrencetan458@gmail.com>
2025-02-22 16:46:28 -0600

    reput files

---
+++ src/main/java/frc/robot/TestRobotContainer.java
@@ -0,0 +1,47 @@
+// package frc.robot;
+
+// import com.ctre.phoenix6.swerve.SwerveModuleConstants;
+// import com.ctre.phoenix6.swerve.SwerveRequest;
+// import com.pathplanner.lib.auto.NamedCommands;
+// import com.pathplanner.lib.commands.PathPlannerAuto;
+// import frc.robot.Constants;
+// import edu.wpi.first.wpilibj2.command.Command;
+// import frc.robot.commands.CommandCollectCoral;
+// import frc.robot.commands.CommandScoreCoral;
+// import frc.robot.commands.TestCommand;
+// import frc.robot.generated.TunerConstants;
+// import frc.robot.subsystems.OneMotorSystem;
+// import frc.robot.subsystems.TestDriveSubsystem;
+
+// public class TestRobotContainer {
+//     OneMotorSystem testSystem = new OneMotorSystem();
+
+
+
+    
+//     TestDriveSubsystem drive = new TestDriveSubsystem(TunerConstants.DrivetrainConstants, TunerConstants.FrontLeft, TunerConstants.FrontRight, TunerConstants.BackLeft, TunerConstants.BackRight);
+
+    
+    
+//     public TestRobotContainer(){
+//         NamedCommands.registerCommand("Test Command", command);
+//         NamedCommands.registerCommand("CommandCollectCoral", collectCommand);
+//         // autoCommand = new PathPlannerAuto("New Auto");  
+//     }
+//     // Command command = new TestCommand(testSystem);
+//     // Command collectCommand = new CommandCollectCoral();
+//     // Command score = new CommandScoreCoral();
+//     // Command autoCommand;
+
+
+// //     public Command getTeleOpCommand(){
+// //          // return command;
+// //     }
+
+// //     public Command getAutoCommand(){
+    
+        
+        
+// //          //return autoCommand;
+// //     }
+// // }
---
+++ src/main/java/frc/robot/commands/TestCommand.java
@@ -0,0 +1,45 @@
+package frc.robot.commands;
+import frc.robot.subsystems.OneMotorSystem;
+
+import java.time.Clock;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class TestCommand extends Command {
+    OneMotorSystem oneMotor;
+    long time;
+
+    public TestCommand(OneMotorSystem oneMotor){
+        this.oneMotor = oneMotor;
+    }
+
+
+
+    
+
+    @Override
+    public void initialize() {
+        time = System.currentTimeMillis();
+        
+    }
+
+    @Override
+    public void execute() {
+        oneMotor.setVoltage(2);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        oneMotor.setVoltage(0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        if((System.currentTimeMillis() - time) > 3000){
+            return true;
+
+        }else{
+            return false;
+        }
+    }
+}
